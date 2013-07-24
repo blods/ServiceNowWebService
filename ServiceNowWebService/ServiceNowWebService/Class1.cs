d2d85c948b10458c3944dc6e4a80f614e45a0290
Dan Mohr <danielmohr91@hotmail.com>
2013-07-24 09:55:44 -0400

    test

---
+++ ServiceNowWebService/ServiceNowWebService/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceNowWebService
+{
+    class Credentials
+    {
+        private static string username;
+        private static string password;
+
+        public Credentials()
+        {
+            try
+            {
+                // Read the file and line by line
+                String[] cred = new String[2];
+
+                System.IO.StreamReader file =
+                   new System.IO.StreamReader("c:\\login.txt");
+                cred[0] = file.ReadLine(); // username
+                cred[1] = file.ReadLine(); // password
+                file.Close();
+
+                username = cred[0];
+                password = cred[1];
+            }
+            catch (Exception e)
+            {
+                // MessageBox.Show("Debugging", "Debugging: " + e.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                username = "admin";
+                password = "password";
+            }
+        }
+
+        public string getUserName()
+        {
+            return username;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+    }
+}
